a54e65f29e02b1a115edccf2e6e7099b8eb2020a
kareem-elemeteny <kareem.elmeteny@gmail.com>
2023-03-21 19:50:36 +0200

    added engin

---
+++ src/engine/Game.java
@@ -0,0 +1,10 @@
+package engine;
+import java.util.ArrayList;
+import model.characters.*;
+import model.world.*;
+public class Game {
+	public static ArrayList<Hero> availableHeros;
+	public static ArrayList<Hero> heros;
+	public static ArrayList<Zombie> zombies;
+	public static Cell [][] map;
+}
