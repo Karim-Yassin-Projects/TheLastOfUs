7118627a07c1e6ed1f3031c97c15ce9b90b8852b
kareem-elemeteny <kareem.elmeteny@gmail.com>
2023-05-20 23:00:26 +0300

    min window width

--- src/views/GameView.java
+++ src/views/GameView.java
@@ -1,8 +1,13 @@
 package views;
 
+import java.awt.Dimension;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 import java.io.IOException;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 import engine.Game;
 import model.characters.Hero;
@@ -13,7 +18,8 @@ public class GameView extends JFrame {
     public GameView() throws IOException {
         super();
         this.setLocation(100, 100);
-        this.setSize(1280, 720);
+        this.setMinimumSize(new Dimension(1280, 720));
+        this.setSize(getMinimumSize());
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setTitle("The Last of Us Legacy");
         HeroSelection heroSelection = new HeroSelection();
@@ -30,7 +36,29 @@ public class GameView extends JFrame {
         });
         this.add(heroSelection);
         this.setVisible(true);
-
+        this.addComponentListener(new ComponentAdapter() {
+            
+            @Override
+            public void componentResized(ComponentEvent e) {
+                SwingUtilities.invokeLater(() -> {
+                    try {
+                        Thread.sleep(20);
+                    } catch (InterruptedException e1) {
+                        
+                    }
+                    int mw = getMinimumSize().width;
+                    int mh = getMinimumSize().height;
+                    int w = getWidth();
+                    int h = getHeight();
+                    if (w < mw || h < mh) {
+                        w = Math.max(w, mw);
+                        h = Math.max(h, mh);
+                        setSize(w, h);
+                    }
+                });
+                
+            }
+        });
     }
 
     public static void main(String[] args) throws IOException {
