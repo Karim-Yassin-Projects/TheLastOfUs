bfe54c15681a4a55596ea66b275c6bd337d116dd
kareem-elemeteny <kareem.elmeteny@gmail.com>
2023-05-22 00:06:19 +0300

    added attack, move and cure button functionlities

--- src/views/ActionsPanel.java
+++ src/views/ActionsPanel.java
@@ -1,9 +1,17 @@
 package views;
 
 import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+
+import engine.Game;
+import exceptions.InvalidTargetException;
+import exceptions.NotEnoughActionsException;
 
 public class ActionsPanel extends JPanel{
     public ActionsPanel() {
@@ -13,15 +21,40 @@ public class ActionsPanel extends JPanel{
         JButton attack = new JButton();
         attack.setBackground(Color.WHITE);
         attack.setText("Attack");
+        attack.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                try{
+                    Game.getSelectedHero().attack();
+                }
+                catch(InvalidTargetException s){
+                    s.getMessage();
+                } catch (NotEnoughActionsException e1) {
+                    e1.getMessage();
+                }
+                
+            }
+        });
         JButton cure = new JButton();
         cure.setBackground(Color.WHITE);
         cure .setText("Cure");
+        cure.addMouseListener(new MouseAdapter() {
+            try{
+                Game.getSelectedHero().cure();
+            }
+            catch(InvalidTargetException e1){
+                e1.getMessage();
+            }
+        });
+
         JButton useSpecial = new JButton();
         useSpecial.setText("Use Special Action");
         useSpecial.setBackground(Color.WHITE);
+        
         JButton endTurn = new JButton();
         endTurn.setText("End Turn");
         endTurn.setBackground(Color.WHITE);
+
         this.add(endTurn);
         this.add(attack);
         this.add(cure);
