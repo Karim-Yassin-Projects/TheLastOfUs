2eaa1170407d5bafe26ef1c962cd052034f15b5f
kareem-elemeteny <kareem.elmeteny@gmail.com>
2023-05-24 21:21:41 +0300

    adjust game over image size

--- src/views/GameOverPanel.java
+++ src/views/GameOverPanel.java
@@ -1,6 +1,10 @@
 package views;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -10,14 +14,49 @@ import engine.Game;
 
 public class GameOverPanel extends JPanel {
     private JLabel label;
+    private final ImageIcon imageIcon;
 
     public GameOverPanel() {
         super();
         this.setLayout(new BorderLayout());
         label = new JLabel();
-        ImageIcon imageIcon = new ImageIcon(Game.checkWin() ? "images/victory.png" : "images/defeat.png");
+        imageIcon = new ImageIcon(Game.checkWin() ? "images/victory.png" : "images/defeat.png");
+        updateImage();
 
-        label.setIcon(imageIcon);
+        this.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                updateImage();
+            }
+        });
         this.add(label, BorderLayout.CENTER);
+        this.setBackground(Color.WHITE);
     }
+
+    private void updateImage() {
+        Image img = imageIcon.getImage();
+        int imageWidth = imageIcon.getIconWidth();
+        int imageHeight = imageIcon.getIconHeight();
+
+        int width = getWidth();
+        int height = getHeight();
+        if (width == 0 || height == 0) {
+            return;
+        }
+        
+        int newWidth;
+        int newHeight;
+
+        if (width * imageHeight > height * imageWidth) {
+            newWidth = width;
+            newHeight = imageHeight * width / imageWidth;
+            
+        } else {
+            newHeight = height;
+            newWidth = imageWidth * height / imageHeight;
+        }
+        img = img.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+        label.setIcon(new ImageIcon(img));
+    }
+
 }
